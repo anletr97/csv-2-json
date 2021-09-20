a1075151ec50647fdcb17b25ed85d9e72bbbd627
anlt <anlt@ait.com>
2021-09-20 17:08:11 +0700

    feat: add execute batch function

--- FMain.Designer.cs
+++ FMain.Designer.cs
@@ -28,30 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FMain));
             this.txtFilePath = new System.Windows.Forms.TextBox();
             this.btnImport = new System.Windows.Forms.Button();
             this.btnExport = new System.Windows.Forms.Button();
             this.btnGen = new System.Windows.Forms.Button();
             this.dgvFields = new System.Windows.Forms.DataGridView();
+            this.colKey = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colVal = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dlgImport = new System.Windows.Forms.OpenFileDialog();
             this.dlgExport = new System.Windows.Forms.SaveFileDialog();
             this.dlgGenerate = new System.Windows.Forms.SaveFileDialog();
-            this.colKey = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colVal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dlgBatchFile = new System.Windows.Forms.OpenFileDialog();
+            this.btnExec = new System.Windows.Forms.Button();
+            this.btnImportJson = new System.Windows.Forms.Button();
+            this.lblFilePath = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFields)).BeginInit();
             this.SuspendLayout();
             // 
             // txtFilePath
             // 
+            this.txtFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.txtFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txtFilePath.Location = new System.Drawing.Point(12, 12);
+            this.txtFilePath.Location = new System.Drawing.Point(13, 322);
             this.txtFilePath.Name = "txtFilePath";
-            this.txtFilePath.Size = new System.Drawing.Size(446, 23);
+            this.txtFilePath.Size = new System.Drawing.Size(445, 23);
             this.txtFilePath.TabIndex = 0;
+            this.txtFilePath.Text = "Click to browse...";
+            this.txtFilePath.Click += new System.EventHandler(this.txtFilePath_Click);
             // 
             // btnImport
             // 
-            this.btnImport.Location = new System.Drawing.Point(302, 42);
+            this.btnImport.Location = new System.Drawing.Point(92, 12);
             this.btnImport.Name = "btnImport";
             this.btnImport.Size = new System.Drawing.Size(75, 36);
             this.btnImport.TabIndex = 1;
@@ -61,7 +69,7 @@
             // 
             // btnExport
             // 
-            this.btnExport.Location = new System.Drawing.Point(221, 42);
+            this.btnExport.Location = new System.Drawing.Point(252, 12);
             this.btnExport.Name = "btnExport";
             this.btnExport.Size = new System.Drawing.Size(75, 36);
             this.btnExport.TabIndex = 2;
@@ -71,11 +79,11 @@
             // 
             // btnGen
             // 
-            this.btnGen.Location = new System.Drawing.Point(383, 42);
+            this.btnGen.Location = new System.Drawing.Point(173, 12);
             this.btnGen.Name = "btnGen";
-            this.btnGen.Size = new System.Drawing.Size(75, 36);
+            this.btnGen.Size = new System.Drawing.Size(73, 36);
             this.btnGen.TabIndex = 3;
-            this.btnGen.Text = "Generate JSON";
+            this.btnGen.Text = "Export JSON";
             this.btnGen.UseVisualStyleBackColor = true;
             this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
             // 
@@ -85,14 +93,10 @@
             this.dgvFields.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colKey,
             this.colVal});
-            this.dgvFields.Location = new System.Drawing.Point(13, 84);
+            this.dgvFields.Location = new System.Drawing.Point(13, 54);
             this.dgvFields.Name = "dgvFields";
             this.dgvFields.Size = new System.Drawing.Size(445, 239);
             this.dgvFields.TabIndex = 4;
-            // 
-            // dlgImport
-            // 
-            this.dlgImport.FileName = "openFileDialog1";
             // 
             // colKey
             // 
@@ -106,18 +110,61 @@
             this.colVal.Name = "colVal";
             this.colVal.Width = 200;
             // 
+            // dlgImport
+            // 
+            this.dlgImport.FileName = "openFileDialog1";
+            // 
+            // dlgBatchFile
+            // 
+            this.dlgBatchFile.FileName = "openFileDialog1";
+            // 
+            // btnExec
+            // 
+            this.btnExec.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExec.Location = new System.Drawing.Point(12, 351);
+            this.btnExec.Name = "btnExec";
+            this.btnExec.Size = new System.Drawing.Size(446, 37);
+            this.btnExec.TabIndex = 5;
+            this.btnExec.Text = "Execute BAT";
+            this.btnExec.UseVisualStyleBackColor = true;
+            this.btnExec.Click += new System.EventHandler(this.btnExec_Click);
+            // 
+            // btnImportJson
+            // 
+            this.btnImportJson.Location = new System.Drawing.Point(13, 12);
+            this.btnImportJson.Name = "btnImportJson";
+            this.btnImportJson.Size = new System.Drawing.Size(73, 36);
+            this.btnImportJson.TabIndex = 6;
+            this.btnImportJson.Text = "Import JSON";
+            this.btnImportJson.UseVisualStyleBackColor = true;
+            this.btnImportJson.Click += new System.EventHandler(this.btnImportJson_Click);
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(11, 306);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(51, 13);
+            this.lblFilePath.TabIndex = 7;
+            this.lblFilePath.Text = "Batch file";
+            // 
             // FMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(475, 450);
+            this.ClientSize = new System.Drawing.Size(471, 407);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.btnImportJson);
+            this.Controls.Add(this.btnExec);
             this.Controls.Add(this.dgvFields);
             this.Controls.Add(this.btnGen);
             this.Controls.Add(this.btnExport);
             this.Controls.Add(this.btnImport);
             this.Controls.Add(this.txtFilePath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FMain";
-            this.Text = "FMain";
+            this.Text = "L2 Auto Test Support Tool";
             ((System.ComponentModel.ISupportInitialize)(this.dgvFields)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -136,5 +183,9 @@
         private System.Windows.Forms.SaveFileDialog dlgGenerate;
         private System.Windows.Forms.DataGridViewTextBoxColumn colKey;
         private System.Windows.Forms.DataGridViewTextBoxColumn colVal;
+        private System.Windows.Forms.OpenFileDialog dlgBatchFile;
+        private System.Windows.Forms.Button btnExec;
+        private System.Windows.Forms.Button btnImportJson;
+        private System.Windows.Forms.Label lblFilePath;
     }
 }
--- FMain.cs
+++ FMain.cs
@@ -1,10 +1,12 @@
 ﻿using CsvHelper;
 using L2AutoTestGenerator.Common;
 using L2AutoTestGenerator.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
@@ -29,18 +31,21 @@ namespace L2AutoTestGenerator
                 filePath = dlgImport.FileName;
             }
 
-            var fields = new List<Field>();
-            using (var reader = new StreamReader(filePath))
-            using (var csv = new CsvReader(reader, System.Globalization.CultureInfo.CurrentCulture))
+            if (filePath != "")
             {
-                // Clear old grid
-                dgvFields.Rows.Clear();
-                var records = csv.GetRecords<Field>();
-                foreach (Field field in records)
+                var fields = new List<Field>();
+                using (var reader = new StreamReader(filePath))
+                using (var csv = new CsvReader(reader, System.Globalization.CultureInfo.CurrentCulture))
                 {
-                    fields.Add(field);
-                    // TODO check and overrided old values
-                    dgvFields.Rows.Add(field.Key, field.Value);
+                    // Clear old grid
+                    dgvFields.Rows.Clear();
+                    var records = csv.GetRecords<Field>();
+                    foreach (Field field in records)
+                    {
+                        fields.Add(field);
+                        // TODO check and overrided old values
+                        dgvFields.Rows.Add(field.Key, field.Value);
+                    }
                 }
             }
         }
@@ -136,6 +141,55 @@ namespace L2AutoTestGenerator
                     }
                 }
             }
+        }
+        #endregion
+
+        private void txtFilePath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlgBatchFile = new OpenFileDialog();
+            dlgBatchFile.Filter = "|*.bat";
+            dlgBatchFile.Title = "Choose execute file path";
+            String filePath = "";
+            if (dlgBatchFile.ShowDialog() == DialogResult.OK)
+            {
+                filePath = dlgBatchFile.FileName;
+            }
+            txtFilePath.Text = filePath;
+        }
+
+        #region Handle excute bat file
+        private void btnExec_Click(object sender, EventArgs e)
+        {
+            if (txtFilePath.Text != "")
+            {
+               Process.Start(txtFilePath.Text);
+            } else
+            {
+                MessageBox.Show("Please insert file path.");
+            }
+        }
+        #endregion
+        #region handle import json
+        private void btnImportJson_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog dlgImport = new OpenFileDialog();
+            //dlgImport.Filter = "|*.json";
+            //dlgImport.Title = "Generate config file to folder";
+            //String filePath = "";
+            //if (dlgImport.ShowDialog() == DialogResult.OK)
+            //{
+            //    filePath = dlgImport.FileName;
+            //}
+            //if (filePath != "")
+            //{
+            //    // deserialize JSON directly from a file
+            //    using (StreamReader file = File.OpenText(filePath))
+            //    {
+            //        JsonSerializer serializer = new JsonSerializer();
+            //        List<Field> movie2 = (List<Field>)serializer.Deserialize(file, typeof(List<Field>));
+            //    }
+            //}
+            MessageBox.Show("Chưa làm đâu, hẹn gặp lại! :v");
         }
         #endregion
     }
